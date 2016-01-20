65c30cac3f6c986847a4230c524e640af180d00d
Aggoh <A_Lyytinen@hotmail.com>
2016-01-20 12:18:05 +0200

    Update Vaatteet.cs

--- Demot/KT6/Vaatteet.cs
+++ Demot/KT6/Vaatteet.cs
@@ -6,9 +6,32 @@ using System.Threading.Tasks;
 
 namespace KT6
 {
-    class Vaatteet
+    class Living
     {
-        public int Koko { get; set; }
-        public int 
+        public string Address { get; set; }
+        public bool Own { get; set; }
+        public bool Rent { get; set; }
+        public int RentAm { get; set; }
+        public int Morgage { get; set; }
+        public int HowMany { get; set; }
+
+        public void PrintData1()
+        {
+            Own = true;
+            Console.WriteLine("Data: Owned : ");
+            Console.WriteLine("Own : " + Own);
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("Mortgage : " + Morgage);
+            Console.WriteLine("You've spent: " + (HowMany * Morgage));
+         }
+        public void PrintData2()
+        {
+            Rent = true;
+            Console.WriteLine("Data: Rented : ");
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("Rent : " + Rent);
+            Console.WriteLine("Rent Amount : " + RentAm);
+            Console.WriteLine("You've spent: " + (HowMany * RentAm));
+        }
     }
 }
