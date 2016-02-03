3c8d0f4c7611b270692b7d8132d6ff3d84cbee7c
Aggoh <A_Lyytinen@hotmail.com>
2016-02-03 15:09:31 +0200

    Everything

---
+++ Demo08/KT2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Shopping> shop = new List<Shopping>();
+            shop.Add(new Shopping {Name = "Kit-Kat ", Price = "2.15" });
+            shop.Add(new Shopping {Name = "Bananananana-na ", Price = "6,89" });
+            shop.Add(new Shopping {Name = "Mudcake ", Price = "95 " });
+
+            Console.WriteLine("Shopping List:");
+            foreach (Shopping items in shop)
+            {
+                Console.WriteLine(items.ToString());
+            }
+        }
+        
+    }
+}
---
+++ Demo08/KT2/Shopping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT2
+{
+    class Shopping
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+       
+        public override string ToString()
+        {
+            return ("Name: " + Name + " Price: " + Price);
+        }
+        
+
+        
+    }
+}
