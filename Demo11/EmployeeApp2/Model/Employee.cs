8d59f4268a12a3083e52fe27eb1d5b7671805f42
Aggoh <A_Lyytinen@hotmail.com>
2016-02-17 15:30:09 +0200

    2.0

---
+++ Demo11/EmployeeApp2/Model/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp2.Model
+{
+    public class Employee
+    {
+        public string ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string JobTitle { get; set; }
+        public EmployeeImage Image { get; set; }
+        public string Fullname
+        {
+            get
+            {
+                return Lastname + " " + Firstname;
+            }
+        }
+    }
+
+    public class EmployeeViewModel
+    {
+        private List<Employee> employees = new List<Employee>();
+        public List<Employee> Employees { get { return employees; } }
+
+        public EmployeeViewModel()
+        {
+            // generate dummy data if needed
+        }
+    }
+}
